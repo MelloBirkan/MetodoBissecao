90e97f5f70f6591016fcc7fc2e50e31ad011accd
Marcello  Gonzatto Birkan <>
2023-04-08 15:37:41 +0000

    Maximo de iterações adicionado

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def bisseccao(f, a, b, epsilon):
+def bisseccao(f, a, b, epsilon, max_iter):
     if f(a) * f(b) >= 0:
         raise ValueError("A função f deve ter sinais opostos em a e b.")
-
-    while (b - a) / 2 > epsilon:
+    
+    for _ in range(max_iter):
         c = (a + b) / 2
-        if f(c) == 0:
-            return c
+        if f(c) == 0 or (b - a) / 2 <= epsilon:
+            break
         elif f(a) * f(c) < 0:
             b = c
         else:
@@ -14,7 +14,7 @@ def bisseccao(f, a, b, epsilon):
     return (a + b) / 2
 
 
-# Exemplo de uso:
+
 if __name__ == "__main__":
     # Função exemplo: f(x) = x^3 - x^2 - 2
     def f(x):
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     b = 2
     epsilon = 1e-1
 
-    raiz = bisseccao(f, a, b, epsilon)
+    raiz = bisseccao(f, a, b, epsilon, 3)
     print(f"A raiz aproximada de f no intervalo [{a}, {b}] com tolerância {epsilon} é {raiz}.")
